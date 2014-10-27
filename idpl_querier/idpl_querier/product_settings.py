9869ca54597bfe8ffc39dca3ae01c0888c119e9d
JarvisZhang <zuowzhang@buaa.edu.cn>
2014-10-27 20:29:36 +0800

    REV: add uwsgi module

--- idpl_querier/idpl_querier/product_settings.py
+++ idpl_querier/idpl_querier/product_settings.py
@@ -23,7 +23,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- idpl_querier/idpl_querier/urls.py
+++ idpl_querier/idpl_querier/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 # from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,7 @@ urlpatterns = patterns('',
 #     url(r'^admin/', include(admin.site.urls)),
     url(r'^perfsonar/', include('querier_server.urls')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
 )
