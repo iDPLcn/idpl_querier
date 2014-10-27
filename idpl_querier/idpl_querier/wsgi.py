556da1c56938237b96b689941e2374cb52268509
JarvisZhang <zuowzhang@buaa.edu.cn>
2014-10-27 10:30:19 +0800

    REV: add productive settings

--- idpl_querier/idpl_querier/wsgi.py
+++ idpl_querier/idpl_querier/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idpl_querier.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idpl_querier.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idpl_querier.product_settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
