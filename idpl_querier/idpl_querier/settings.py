82289b8b3a1d4314bfe6845d9c30944e0e7dc51d
JarvisZhang <zuowzhang@buaa.edu.cn>
2015-01-05 21:48:42 +0800

    REV: update url

--- idpl_querier/idpl_querier/settings.py
+++ idpl_querier/idpl_querier/settings.py
@@ -90,4 +90,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/perfsonar/static/'
+STATIC_URL = '/idpl_querier/static/'
--- idpl_querier/idpl_querier/urls.py
+++ idpl_querier/idpl_querier/urls.py
@@ -7,11 +7,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'idpl_querier.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^condor/', include('condor_archive.urls')),
-    url(r'^perfsonar/', include('querier_server.urls')),
-    url(r'^perfsonar/docs/', include('rest_framework_swagger.urls')),
-    url(r'^perfsonar/static/(?P<path>.*)$', 'django.views.static.serve', {
+    url(r'^idpl_querier/admin/', include(admin.site.urls)),
+    url(r'^idpl_querier/docs/', include('rest_framework_swagger.urls')),
+    url(r'^idpl_querier/perfsonar/static/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.STATIC_ROOT,
         }),
+    url(r'^condor/', include('condor_archive.urls')),
+    url(r'^perfsonar/', include('querier_server.urls')),
 )
